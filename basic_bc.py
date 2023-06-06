127a7d2bc8b1825a903387d96826337ba4bcca56
yunokiisshin <yunokiisshin@gmail.com>
2023-06-06 17:09:11 +0900

    basic function started

--- basic_bc.py
+++ basic_bc.py
@@ -1,12 +1,13 @@
-from music21 import chord, stream, note, midi
+from music21 import *
+from chord_map import *
 
-def chord_progression_to_midi(chord_progression, filename):
-    # Create a music21 stream object to hold the notes and chords
+def chord_progression_to_midi(chord_progression, note_length, filename):
+    # Create an empty music21 stream object to hold the notes and chords
     music_stream = stream.Stream()
 
     for chords in chord_progression:
         # For each chord in the progression, create a music21 chord object
-        c = chord.Chord(chords)
+        c = chord.Chord(chords, duration=duration.Duration(note_length))
         # Add the chord to the stream
         music_stream.append(c)
 
@@ -16,7 +17,10 @@ def chord_progression_to_midi(chord_progression, filename):
     mf.write()
     mf.close()
 
-# Test with a simple chord progression
-chord_progression = [["C4", "E4", "G4"], ["F4", "A4", "C5"], ["G4", "B4", "D5"], ["C4", "E4", "G4"]]
-filename = "chord_progression.mid"
-chord_progression_to_midi(chord_progression, filename)
+def main(note_length = 2.0, chord_progression = [["C4", "E4", "G4"], ["F4", "A4", "C5"], ["G4", "B4", "D5"], ["C4", "E4", "G4"]]):
+    # Test with a simple chord progression
+    filename = "./result/chord_progression.mid"
+    chord_progression_to_midi(chord_progression, note_length, filename)
+    
+if __name__ == "__main__":
+    main()
