a7166bb02f979e5d06022067ab8b03bfdb255517
yunokiisshin <yunokiisshin@gmail.com>
2023-06-07 09:56:35 +0900

    chords

--- chord_map.py
+++ chord_map.py
@@ -51,10 +51,81 @@ def minor_triad(root):
     return voicing
 
 def major_seventh(root):
-    return [root, note_shift(root, 4), note_shift(root, 7), note_shift(root, 11)]
+     # Define the notes of the chord
+    third = root + 4
+    fifth = root + 7
+    seventh = root + 11
+
+    # Define possible voicings
+    voicings = [
+        [root, third, fifth, seventh],     # root position
+        [root, fifth, seventh, third+12], 
+        [root, seventh, third+12, fifth+12],  
+        [third, fifth, seventh, root+12],       # first inversion
+        [third, seventh, root+12, fifth+12],       
+        [third, seventh, root+12, fifth+12],       
+        [fifth - 12, seventh-12, root, third],  # second inversion
+        [fifth - 12, root, third, fifth], 
+        [seventh-12, root, third, fifth]
+        # Add more voicings as desired...
+    ]
+    
+    # Randomly select a voicing
+    voicing = random.choice(voicings)
+
+    return voicing
+
+
 
 def minor_seventh(root):
-    return [root, note_shift(root, 3), note_shift(root, 7), note_shift(root, 10)]
+     # Define the notes of the chord
+    third = root + 3
+    fifth = root + 7
+    seventh = root + 10
+
+    # Define possible voicings
+    voicings = [
+        [root, third, fifth, seventh],     # root position
+        [root, fifth, seventh, third+12], 
+        [root, seventh, third+12, fifth+12],  
+        [third, fifth, seventh, root+12],       # first inversion
+        [third, seventh, root+12, fifth+12],       
+        [third, seventh, root+12, fifth+12],       
+        [fifth - 12, seventh-12, root, third],  # second inversion
+        [fifth - 12, root, third, fifth], 
+        [seventh-12, root, third, fifth]
+        # Add more voicings as desired...
+    ]
+    
+    # Randomly select a voicing
+    voicing = random.choice(voicings)
+
+    return voicing
+
+
 
 def dominant_seventh(root):
-    return [root, note_shift(root, 4), note_shift(root, 7), note_shift(root, 10)]
+     # Define the notes of the chord
+    third = root + 4
+    fifth = root + 7
+    seventh = root + 10
+
+    # Define possible voicings
+    voicings = [
+        [root, third, fifth, seventh],     # root position
+        [root, fifth, seventh, third+12], 
+        [root, seventh, third+12, fifth+12],  
+        [third, fifth, seventh, root+12],       # first inversion
+        [third, seventh, root+12, fifth+12],       
+        [third, seventh, root+12, fifth+12],       
+        [fifth - 12, seventh-12, root, third],  # second inversion
+        [fifth - 12, root, third, fifth], 
+        [seventh-12, root, third, fifth]
+        # Add more voicings as desired...
+    ]
+    
+    # Randomly select a voicing
+    voicing = random.choice(voicings)
+
+    return voicing
+
